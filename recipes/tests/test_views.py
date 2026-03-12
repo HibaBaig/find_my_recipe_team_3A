013f3e2f735abe8315d95a51e91690a46ddc0b51
“Cameron <2901899s@student.gla.ac.uk>
2026-03-12 15:07:55 +0000

    tests1.4

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from recipes.models import Recipe
 
 
-class TestHomeView(TestCase):
+class TestViews(TestCase):
     def test_home_view_template(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
@@ -16,7 +16,11 @@ class TestHomeView(TestCase):
             description="description",
             steps="steps"
         )
-
-
+        
         response = self.client.get(reverse("home"))
         self.assertIn(recipe, response.context["recipes"])
+
+    def test_toggle_save_view(self):
+        
+
+    
